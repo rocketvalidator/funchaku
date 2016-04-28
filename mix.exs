9d8a8fb76a5bb75ef999045d30d6249ca139805b
Jaime Iniesta <jaimeiniesta@gmail.com>
2016-04-28 11:45:52 +0200

    adds dependencies

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Funchaku.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,9 @@ defmodule Funchaku.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      httpoison: "~> 0.8",
+      poison:    "~> 1.5"
+    ]
   end
 end
