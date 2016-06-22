adedf029c93ee76f74163a75f3b2c8b4bee2a287
Jaime Iniesta <jaimeiniesta@gmail.com>
2016-06-23 00:31:39 +0200

    release 0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Funchaku.Mixfile do
 
   def project do
     [app: :funchaku,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.3",
      description: "Elixir client for the Nu HTML Checker",
      package: package,
